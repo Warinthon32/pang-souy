cb337bc9961b57358148699d451c291cf64bd8a2
warinthon pungkhamnoi <warinthon@MacBook-Air-khxng-warinthon.local>
2024-11-07 18:41:06 +0700

    0.02

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-// var product = [{
-//     id: 1,
-//     img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
-//     name: 'Nike',
-//     price: 7000,
-//     description: 'Nike Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
-//     type: 'shoe'
-// }, {
-//     id: 2,
-//     img: 'https://images.unsplash.com/photo-1511746315387-c4a76990fdce?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
-//     name: 'Adidas shirt',
-//     price: 1500,
-//     description: 'Adidas shirt Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
-//     type: 'shirt'
-// }, {
-//     id: 3,
-//     img: 'https://images.unsplash.com/photo-1593287073863-c992914cb3e3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-//     name: 'Adidas shoe',
-//     price: 45000,
-//     description: 'Adidas shoe Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
-//     type: 'shoe'
-// }];
+ var product = [{
+     id: 1,
+     img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
+     name: 'Nike',
+     price: 7000,
+     description: 'Nike Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+     type: 'shoe'
+ }, {
+     id: 2,
+     img: 'https://images.unsplash.com/photo-1511746315387-c4a76990fdce?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
+     name: 'Adidas shirt',
+     price: 1500,
+     description: 'Adidas shirt Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+     type: 'shirt'
+ }, {
+     id: 3,
+    img: 'https://images.unsplash.com/photo-1593287073863-c992914cb3e3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
+     name: 'Adidas shoe',
+     price: 45000,
+     description: 'Adidas shoe Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+     type: 'shoe'
+ }];
 
 var product;
 
-// [{},{},{}] // length = 3
+ [{},{},{}];  length = 3
 
 $(document).ready(() => {
 
@@ -61,7 +61,7 @@ function numberWithCommas(x) {
 }
 
 function searchsomething(elem) {
-    // console.log('#'+elem.id)
+    console.log('#'+elem.id)
     var value = $('#'+elem.id).val()
     console.log(value)
 
